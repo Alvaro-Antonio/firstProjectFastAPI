d7f0a7d838077cf29189701512f9e3eba033791f
Álvaro Antônio <alvaroantoniosbu@gmail.com>
2024-01-09 00:33:57 -0300

    Add converter coin sync

---
+++ firstProjectFastAPi/converter.py
@@ -0,0 +1,26 @@
+from os import getenv
+import requests
+from fastapi import HTTPException
+
+
+ALPHAVANTAGEAPIKEY = getenv('API_KEY')
+
+def sync_conveter(from_currency: str, to_currency: str, price: float):
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGEAPIKEY}'
+
+    try:
+        response = requests.get(url=url)
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=error)
+    
+    data = response.json()
+
+    print(data)
+    print("\n\n")
+
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate not is in response')
+    
+    exchange_rate = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+    return price * exchange_rate
